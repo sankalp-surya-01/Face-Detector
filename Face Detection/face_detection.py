25b26c1afbe62152cdbc829d09ed3cd504cdeb4a
sankalp-surya-01 <99335402+sankalp-surya-01@users.noreply.github.com>
2024-08-26 12:21:04 +0530

    Add files via upload

---
+++ Face Detection/face_detection.py
@@ -0,0 +1,23 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
+    face_count = len(faces)
+    cv2.putText(frame, f'Faces detected: {face_count}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 50, 255), 2)
+
+    cv2.imshow('Face Detection', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
